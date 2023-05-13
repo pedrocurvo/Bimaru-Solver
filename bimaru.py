5557c9bfb3e04e1d03bfd0fa14c665be154728cb
pedrocurvo <95446270+pedrocurvo@users.noreply.github.com>
2023-05-13 19:56:52 +0100

    V1.3

--- bimaru.py
+++ bimaru.py
@@ -23,8 +23,13 @@ class BimaruState:
     state_id = 0
 
     def __init__(self, board):
+        if BimaruState.state_id == 0:
+            Bimaru.fill_water_rows_cols(board)
+            Bimaru.fill_water_around_ships(board)
         self.board = board
         self.id = BimaruState.state_id
+        self.expectated_ships = [4, 3, 2, 1]
+        self.ships = [0, 0, 0, 0]
         BimaruState.state_id += 1
         self.grids = [board]
 
@@ -40,7 +45,7 @@ class BimaruState:
 class Board:
     """Representação interna de um tabuleiro de Bimaru."""
 
-    def __init__(self, board: np.ndarray, col_number: np.ndarray, row_number: np.ndarray):
+    def __init__(self, board: np.ndarray, col_number = None, row_number = None):
         """Construtor da classe. Recebe um array bidimensional
         (matriz) numpy com o conteúdo do tabuleiro."""
         self.board = board
@@ -99,6 +104,20 @@ class Board:
         for row in self.board:
             print(" ".join(row))
 
+    def match_boards(self, other):
+        """Verifica se dois tabuleiros encaixam."""
+        for row in range(10):
+            for col in range(10):
+                if self.board[row][col].upper() == other.board[row][col].upper():
+                    continue
+                elif self.board[row][col] == '' or other.board[row][col] == '':
+                    continue
+                else:
+                    return False
+            return True
+        # Falta contar peças para ver se encaixa
+    
+
     @staticmethod
     def parse_instance():
         """Lê o test do standard input (stdin) que é passado como argumento
@@ -107,10 +126,10 @@ class Board:
         board = np.zeros((10, 10), dtype=str)
         # Read the first line of txt file
         line = sys.stdin.readline().split()
-        col = np.array([int(x) for x in line[1:]])
+        row = np.array([int(x) for x in line[1:]])
         # Read the second line of txt file
         line = sys.stdin.readline().split()
-        row = np.array([int(x) for x in line[1:]])
+        col = np.array([int(x) for x in line[1:]])
         # Number of Hints
         n_hints = int(sys.stdin.readline())
         # Read the hints
@@ -126,6 +145,8 @@ class Board:
 class Bimaru(Problem):
     def __init__(self, board: Board):
         """O construtor especifica o estado inicial."""
+        Bimaru.fill_water_rows_cols(board)
+        Bimaru.fill_water_around_ships(board)
         self.board = board
         # TODO
         pass
@@ -133,6 +154,15 @@ class Bimaru(Problem):
     def actions(self, state: BimaruState):
         """Retorna uma lista de ações que podem ser executadas a
         partir do estado passado como argumento."""
+        if state.ships[3] != state.expectated_ships[3]:
+            pass
+        if state.ships[2] != state.expectated_ships[2]:
+            pass
+        if state.ships[1] != state.expectated_ships[1]:
+            pass
+        if state.ships[0] != state.expectated_ships[0]:
+            pass
+        # add water to empty positions
         # TODO
         pass
 
@@ -160,6 +190,71 @@ class Bimaru(Problem):
         """Função heuristica utilizada para a procura A*."""
         # TODO
         pass
+    
+    @staticmethod
+    def fill_water_rows_cols(board: Board):
+        """Preenche com água as linhas e colunas que não contêm barcos."""
+        for row in range(10):
+            if board.row_number[row] == 0:
+                for col in range(10):
+                    board.board[row][col] = 'w'
+        for col in range(10):
+            if board.col_number[col] == 0:
+                for row in range(10):
+                    board.board[row][col] = 'w'
+
+    @staticmethod
+    def fill_water_around_ship(board: Board):
+        """Preenche com água as posições em redor de um barco."""
+        for row in range(10):
+            for col in range(10):
+                # Fill around C 
+                if board.board[row][col] in ('c', 'C'):
+                    for i in range(row - 1, row + 2):
+                        for j in range(col - 1, col + 2):
+                            if i >= 0 and i < 10 and j >= 0 and j < 10:
+                                if board.board[i][j] not in ('c', 'C'):
+                                    board.board[i][j] = 'w'
+                # Fill around M
+                if board.board[row][col] in ('m', 'M'):
+                    for i in range(row - 1, row + 2):
+                        for j in range(col - 1, col + 2):
+                            if i >= 0 and i < 10 and j >= 0 and j < 10:
+                                if board.board[i][j] not in ('c', 'C') and j != col and i != row:
+                                    board.board[i][j] = 'w'
+
+                # Fill around B
+                if board.board[row][col] in ('b', 'B'):
+                    for i in range(row - 2, row + 2):
+                        for j in range(col - 1, col + 2):
+                            if i >= 0 and i < 10 and j >= 0 and j < 10:
+                                if board.board[i][j] not in ('b', 'B') and j != col or i > row:
+                                    board.board[i][j] = 'w'
+                # Fill around T
+                if board.board[row][col] in ('t', 'T'):
+                    for i in range(row - 1, row + 3):
+                        for j in range(col - 1, col + 2):
+                            if i >= 0 and i < 10 and j >= 0 and j < 10:
+                                if board.board[i][j] not in ('t', 'T') and j != col or i < row:
+                                    board.board[i][j] = 'w'
+                # Fill around L
+                if board.board[row][col] in ('l', 'L'):
+                    for i in range(row - 1, row + 2):
+                        for j in range(col - 1, col + 3):
+                            if i >= 0 and i < 10 and j >= 0 and j < 10:
+                                if board.board[i][j] not in ('l', 'L') and i != row or j < col:
+                                    board.board[i][j] = 'w'
+                # Fill around R
+                if board.board[row][col] in ('r', 'R'):
+                    for i in range(row - 1, row + 2):
+                        for j in range(col - 2, col + 2):
+                            if i >= 0 and i < 10 and j >= 0 and j < 10:
+                                if board.board[i][j] not in ('r', 'R') and i != row or j > col:
+                                    board.board[i][j] = 'w'
+                    
+                        
+
+
 
     # TODO: outros metodos da classe
 
@@ -167,7 +262,11 @@ class Bimaru(Problem):
 if __name__ == "__main__":
     # Ler a instância a partir do ficheiro 'i1.txt' (Figura 1): # $ python3 bimaru.py < i1.txt
     board = Board.parse_instance()
-    board.print()
+    Bimaru.fill_water_rows_cols(board)
+    Bimaru.fill_water_around_ship(board)
+    #board.print()
+    print(board.board)
+    '''
     # Imprimir valores adjacentes
     print(board.adjacent_vertical_values(3, 3))
     print(board.adjacent_horizontal_values(3, 3))
@@ -180,6 +279,7 @@ if __name__ == "__main__":
     test[0][2] = 'w'
     s1 = problem.result(s0, test)
     s1.print()
+    '''
     # TODO:
     # Ler o ficheiro do standard input,
     # Usar uma técnica de procura para resolver a instância,
